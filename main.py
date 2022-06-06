9959da6a1d487bccd468b78832a8f994e23e8a24
Nick Ahn <n_ahn2@u.pacific.edu>
2022-06-05 19:03:30 -0700

    refactored api code into class

--- main.py
+++ main.py
@@ -35,99 +35,89 @@ def refreshAccessToken():
     return token
 
 
-# GET request to get User's Top Items
-# Param: string:accessToken, str:time_range
-# returns a json object with User's Top Tracks
-def getUserTopItems(accessToken, time_range):
-    endpoint = f"https://api.spotify.com/v1/me/top/tracks"
-
-    header = {
-        "Authorization": "Bearer " + accessToken
-    }
-
-    queryParameters = {
-        'time_range': time_range
-    }
-
-    # GET request
-    response = requests.get(url=endpoint, headers=header, params=queryParameters)
-    if response.status_code != 200:
-        print("Error: status code ", response.status_code)
-        return
-
-    topItems = response.json()
-    return topItems
-
-
-def createPlaylist(token, user_id, playlist_name, is_public, description):
-    endpoint = f"https://api.spotify.com/v1/users/{user_id}/playlists"
-    header = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {token}"
-    }
-
-    request_body = {
-        "name":playlist_name,
-        "public":is_public,
-        "description": description
-    }
-
-    response = requests.post(url=endpoint, headers=header, data=json.dumps(request_body))
-    if response.status_code != 200:
-        print("Error: status code ", response.status_code)
-        return
-
-    json_data = response.json()
-    return json_data
-
-
-# POST request to Add one or more items to a user's playlist
-# Params: string:token; string:playlist_id; list:tracks = list of track uri's
-# Return: a snapshot ID for the playlist
-def addSongsToPlaylist(token, playlist_id, tracks):
-    endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-
-    header = {
+class apiHandler():
+    def __init__(self, token) -> None:
+        self.token = token
+        self.header = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {token}"
-    }
+            "Authorization": "Bearer " + self.token
+        }
 
-    query = {
-        'uris':tracks
-    }
+    # GET request to get User's Top Items
+    # Param: string:accessToken, str:time_range
+    # returns a json object with User's Top Tracks
+    # TODO: test with new header
+    def getUserTopItems(self, time_range):
+        endpoint = f"https://api.spotify.com/v1/me/top/tracks"
 
-    ## TODO: Fix Error parsing JSON
-    # request_body = {
-    #     'uris':json.dumps(tracks)
-    # }
+        queryParameters = {
+            'time_range': time_range
+        }
 
-    response = requests.post(url=endpoint, headers=header, params=query)
-    print(response)
+        # GET request
+        response = requests.get(url=endpoint, headers=self.header, params=queryParameters)
+        if response.status_code != 200:
+            print("Error: status code ", response.status_code)
+            print(response)
+            return
 
-    json_data = response.json()
-    print(type(json_data))
-    print(json_data)
-    return json_data
- 
-    
+        topItems = response.json()
+        return topItems
+
+    def createPlaylist(self, user_id, playlist_name, is_public=True, description=""):
+        endpoint = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+        
+        request_body = {
+            "name":playlist_name,
+            "public":is_public,
+            "description": description
+        }
+
+        response = requests.post(url=endpoint, headers=self.header, data=json.dumps(request_body))
+        #TODO: add error handling (try/catch?)
+
+        json_data = response.json()
+        return json_data
+
+    # POST request to Add one or more items to a user's playlist
+    # Params: string:token; string:playlist_id; list:tracks = list of track uri's
+    # Return: a snapshot ID for the playlist
+    def addSongToPlaylist(self, playlist_id, tracks):
+        endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+        query = {
+            'uris':tracks
+        }
+
+        ## TODO: Fix Error parsing JSON
+        # request_body = {
+        #     'uris':json.dumps(tracks)
+        # }
+
+        response = requests.post(url=endpoint, headers=self.header, params=query)
+        print(response)
+
+        json_data = response.json()
+        print(type(json_data))
+        print(json_data)
+        return json_data
 
 
+api = apiHandler(refreshAccessToken())
+topItems = api.getUserTopItems("short_term")
+playlist_id = "6C95koZYc4e8qG1Fnu2C25"
 
-token = refreshAccessToken()
-topItems = getUserTopItems(token, "long_term")
-# print(type(topItems))
+# songs_uris = ''
+# for i in topItems['items']:
+#     # print(i['uri'])
+#     # print(i['name'])
+#     # print(i['id'], end="\n\n")
+#     songs_uris += i['uri']
+#     #TODO: find a more elegant way to do this
+#     if(i != topItems['items'][-1]):
+#         songs_uris += ","
 
-songs_uris = ''
-for i in topItems['items']:
-    # print(i['uri'])
-    # print(i['name'])
-    # print(i['id'], end="\n\n")
-    songs_uris += i['uri']
-    #TODO: find a more elegant way to do this
-    if(i != topItems['items'][-1]):
-        songs_uris += ","
-
-addSongsToPlaylist(token, "6C95koZYc4e8qG1Fnu2C25", songs_uris)
+# addSongsToPlaylist(token, "6C95koZYc4e8qG1Fnu2C25", songs_uris)
 
 # createPlaylist(token, "31gdfhbyzslosy6wycwpllk5ab6q", "test", True, "Playlist created with Spotify API")
 # print(json.dumps(topItems, indent=2))
